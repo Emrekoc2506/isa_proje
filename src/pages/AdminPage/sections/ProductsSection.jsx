c3f261ff0680d661b61998c35c785bfcab29c478
corsa46 <emrekoc4615@gmail.com>
2026-08-06 17:40:44 +0300

    fix(products): add missing FiX icon import in ProductsSection.jsx to fix Uncaught ReferenceError

--- src/pages/AdminPage/sections/ProductsSection.jsx
+++ src/pages/AdminPage/sections/ProductsSection.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { 
   FiTrash2, FiEdit3, FiPlus, FiGrid, FiList, FiAlertCircle, FiLock, FiUnlock,
-  FiTag, FiDollarSign, FiImage, FiSliders, FiChevronLeft, FiChevronRight, FiCheck, FiUploadCloud, FiBox, FiFileText
+  FiTag, FiDollarSign, FiImage, FiSliders, FiChevronLeft, FiChevronRight, FiCheck, FiUploadCloud, FiBox, FiFileText, FiX
 } from 'react-icons/fi';
 import { motion, AnimatePresence } from 'framer-motion';
 import * as productApi from '../../../services/productApi';
